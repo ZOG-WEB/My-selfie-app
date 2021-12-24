7d7caf06ad9b8755c3da0c979309cb0a805e45b6
ZOG-WEB <79850764+ZOG-WEB@users.noreply.github.com>
2021-12-24 12:51:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
  var SpeechRecognition=window.webkitSpeechRecognition;
 
- var recognition= new SpeechRecognition;
+ var recognition= new SpeechRecognition();
  
  function start(){
 document.getElementById("textbox").innerHTML="";
@@ -12,13 +12,27 @@ recognition.start()
  var Content=event.results[0][0].transcript;
  document.getElementById("textbox").innerHTML=Content;
  console.log(Content);
- 
+ if(Content=="take my selfie")
+ {
+     console.log("taking selfie");
+     speak();
 
- 
+ }
+ }
+ function speak(){
+     var synth=window.speechSynthesis;
+     speak_data=document.getElementById("textbox").value;
+     var utterthis=new SpeechSynthesisUtterance(speak_data);
+     synth.speak(utterthis);
+     Webcam.attach(camera);
  }
 
- 
-
-
+camera=document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
 
 
